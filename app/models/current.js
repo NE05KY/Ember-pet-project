01747e306fa7f70713dc09975e9ea561e476a170
Alexey Alexeev <aaliakseyeu@exadel.com>
2016-08-04 08:16:18 +0300

    Fix current object model

--- app/models/current.js
+++ app/models/current.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	date: DS.attr('date'),
-	rates: DS.attr('object'),
+	rates: DS.attr(),
 	chunkedRates: function () {
 		var arrays = [[],[],[]];
 
